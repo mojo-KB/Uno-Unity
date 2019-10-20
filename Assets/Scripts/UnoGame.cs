c6c2bfd8316bfe9507bb2b9862b8122b71ae8c77
Seni-J <36042348+Seni-J@users.noreply.github.com>
2019-10-21 01:33:39 +0200

    Initial commit

---
+++ Assets/Scripts/UnoGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnoGame : MonoBehaviour
+{
+    private enum UnoCardsColors
+    {
+        Red,
+        Blue,
+        Yellow,
+        Green,
+        Black
+    }
+
+    private enum UnoCardsValues
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Block,
+        Reverse,
+        AddTwo,
+        AddFour
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
