b4423014cdba9023d3f2ff9a5f8dac4b062583c2
Senistan Jegarajasingam <36042348+Seni-J@users.noreply.github.com>
2019-11-28 01:49:17 +0100

    Added script for moving cards

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHold = false;
+
+    Vector3 mousePos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHold)
+        {
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        startPosX = mousePos.x - this.transform.localPosition.x;
+        startPosY = mousePos.y - this.transform.localPosition.y;
+
+        isBeingHold = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHold = false;
+    }
+
+}
--- Assets/Scripts/PlayerHand.cs
+++ Assets/Scripts/PlayerHand.cs
@@ -8,15 +8,33 @@ public class PlayerHand : MonoBehaviour
     List<UnoCards> hand = new List<UnoCards>();
     List<UnoCards> set;
 
+    bool loading = true;
+
     // Start is called before the first frame update
     void Start()
+    {
+        LoadHand();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hand.Count == 0 && loading)
+        {
+            LoadHand();
+            loading = false;
+        }
+
+
+    }
+    
+    void LoadHand()
     {
         set = GameObject.FindWithTag("Deck").GetComponent<UnoCardsSet>().cardsSet;
         hand = set.Take(7).ToList();
         Sprite[] cardSprites;
         int x = -5;
         int y = -4;
-
 
         cardSprites = Resources.LoadAll<Sprite>("Sprites/Cards");
         foreach (var card in hand)
@@ -27,6 +45,8 @@ public class PlayerHand : MonoBehaviour
                 {
                     Vector3 newpos = new Vector3(x, y, 0);
                     GameObject go = new GameObject("Card " + card.value.ToString() + " " + card.color.ToString());
+                    go.AddComponent<Move>();
+                    go.AddComponent<BoxCollider2D>();
                     SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
                     renderer.sprite = sprite;
                     renderer.transform.position = newpos;
@@ -34,11 +54,5 @@ public class PlayerHand : MonoBehaviour
                 }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
