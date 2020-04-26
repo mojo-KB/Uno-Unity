ec9b0fe95c01dbb56a8f2403158c00a9b1916c1d
Seni-J <senistan.jegarajasingam@cpnv.ch>
2020-04-26 02:34:34 +0200

    Add label to know who needs to play

---
+++ Assets/Scripts/TurnText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TurnText : MonoBehaviour
+{
+    public UnoCardsSet deck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck = GameObject.Find("Deck").GetComponent<UnoCardsSet>();
+        if(deck.state == GameState.PLAYERTURN)
+        {
+            gameObject.GetComponent<TMP_Text>().text = "C'est au tour du joueur";
+        }
+        else
+        {
+            gameObject.GetComponent<TMP_Text>().text = "C'est au tour de l'IA";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (deck.state == GameState.PLAYERTURN)
+        {
+            gameObject.GetComponent<TMP_Text>().text = "C'est au tour du joueur";
+        }
+        else
+        {
+            gameObject.GetComponent<TMP_Text>().text = "C'est au tour de l'IA";
+        }
+    }
+}
